f8f00f98914690944fd245cab617fe32bf3b9fe1
Julian De Tomasi <juliandetomasi@gmail.com>
2020-06-02 18:40:59 -0300

    Updates

---
+++ precache-manifest.39cd05a970acca8d3924b723a4085f03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dcad18130f54ea12656c41e1b1babff",
+    "url": "/deck-view/index.html"
+  },
+  {
+    "revision": "cc5f993adbc303a2582a",
+    "url": "/deck-view/static/css/main.33f3395e.chunk.css"
+  },
+  {
+    "revision": "6ae37ec1e7201a54e0d8",
+    "url": "/deck-view/static/js/2.ffd3838c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/deck-view/static/js/2.ffd3838c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc5f993adbc303a2582a",
+    "url": "/deck-view/static/js/main.2837ca76.chunk.js"
+  },
+  {
+    "revision": "e5a6c9d825a87f331196",
+    "url": "/deck-view/static/js/runtime-main.011c297e.js"
+  }
+]);
